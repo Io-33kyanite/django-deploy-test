de66d5451f18040a2da97d1ef8fb9dc1ef2c148f
sasaki <iorisasaki1234@gmail.com>
2021-07-14 17:32:01 +0900

    db construction

--- myproject/myblog/models.py
+++ myproject/myblog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from mdeditor.fields import MDTextField
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    published = models.DateTimeField()
+    image = models.ImageField(upload_to = 'media/')
+    body = MDTextField()
+
+    def __str__(self):
+        return self.title
