ff2dc28dc297d06ed5af4aa62aa4457ed7e938de
sasaki <iorisasaki1234@gmail.com>
2021-07-14 17:48:52 +0900

    blog app complete v1.0

--- myproject/myblog/views.py
+++ myproject/myblog/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+from django.shortcuts import get_object_or_404
 
 def index(request):
     # return HttpResponse("Hello Django!!")
-    return render(request, 'myblog/index.html')
+    # return render(request, 'myblog/index.html')
+    posts = Post.objects.order_by('-published')
+    return render(request, 'myblog/index.html', {'posts': posts})
+
+def showBody(request, id):
+    object = get_object_or_404(Post, pk=id)
+    post = {
+        'post':object,
+    }
+    return render(request, 'myblog/body.html', post)
